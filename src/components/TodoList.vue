fa2d5d104eb8c0a180444a10e687011cda5e8d52
Oana Munteanu <anaom@Oanas-MBP.home>
2018-04-29 21:56:30 +0100

    Tutorial vue 2 TodoList - initial commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <ul>
+      <li>To Do 1</li>
+      <li>To Do 2</li>
+      <li>To Do 3</li>
+    </ul>
+  </div>
+</template>
+
+
+<script type="text/javascript">
+export default {
+
+};
+
+</script>
+<style>
+</style>
